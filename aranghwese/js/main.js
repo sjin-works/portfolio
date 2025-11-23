d78ba252ccaaaad2074ef13dd7d34493c27a875c
ssj2540 <ssj2540@gmail.com>
2025-11-23 17:50:18 +0900

    251123

--- aranghwese/js/main.js
+++ aranghwese/js/main.js
@@ -33,4 +33,20 @@ $(document).ready(function (){
         $(this).hide()
         $('.visual .ctrl_wrap .btn_stop').show()
     })
+    /************************ visual swiper ************************/
+
+
+    AOS.init({
+        offset: 150, // 해당 콘텐츠가 하단에서 몇 px 위로 올라와에 나타나는 효과가 나타날지 셋팅하는 값
+        duration: 800, // 애니메이션 효과가 작동되는 시간
+        easing: 'ease', // 가속도
+    });
+    $(window).scroll(function(){
+        AOS.init({
+            offset: 150, // 해당 콘텐츠가 하단에서 몇 px 위로 올라와에 나타나는 효과가 나타날지 셋팅하는 값
+            duration: 800, // 애니메이션 효과가 작동되는 시간
+            easing: 'ease', // 가속도
+    
+        });
+    });
 })//ready
